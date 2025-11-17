ae7d1b624a91c7a50b23a90b48b12467d83d95f9
Hannn0206 <s1120295@dmhs.kh.edu.tw>
2025-11-17 10:24:04 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+int main() {
+    double celsius;
+    double fahrenheit
+    std::cout<<"請輸入華氏溫度!\n";
+    std:: std::cin >> fahrenheit;
+    celsius = (fahrenheit - 32)/9*5;
+    std::cout<<"攝氏溫度為: " << celsius << " 度\n"
+    return 0;
+}
