4ffe61f4793402cdd84ac4791fa69bd25cff6587
Hannn0206 <s1120295@dmhs.kh.edu.tw>
2025-11-17 10:57:50 +0800

    Add files via upload

---
+++ hw6.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    float grade〔5〕〔5〕= {
+                              {1,90,80,90,86,7},
+                              {2,99,70,85,84,7},
+                              {3,77,65,57,66,3},
+                              {4,61,87,88,78,7},
+                              {5,50,65,77,64}
+                           };
+                           
+    cout << "請輸入座號以查詢成績:";
+    cin >> numbeer;
+    cout << number << "號同學的各科成績為:" << end1;
+    cout << "國文\t英文\t數學\t自然" << end1;
+    
+
+    for (int j = 1; j < 5; j++)
+    cout << " " << grade〔number-1〕〔j〕<< '\t';
+}
